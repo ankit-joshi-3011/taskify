39bb71a11f12e4b05f71746231271c6a1394062a
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-13 21:49:10 +0530

    Once the POST request is successful, sent the 201 created status code and sent the location of the created user in the Location header of the response

--- taskify/src/main/java/com/taskify/user/controller/UserController.java
+++ taskify/src/main/java/com/taskify/user/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.taskify.user.controller;
 
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.taskify.entity.User;
 import com.taskify.user.service.UserService;
@@ -18,7 +22,16 @@ public class UserController {
 	}
 
 	@PostMapping("/register")
-	public void save(@RequestBody User user) {
-		userService.save(user);
+	public ResponseEntity<User> save(@RequestBody User user) {
+		User savedUser = userService.save(user);
+
+		URI savedUserLocation = ServletUriComponentsBuilder
+			.fromCurrentRequest()
+			.replacePath("/api/users")
+			.path("/{id}")
+			.buildAndExpand(savedUser.getId())
+			.toUri();
+
+		return ResponseEntity.created(savedUserLocation).build();
 	}
 }
