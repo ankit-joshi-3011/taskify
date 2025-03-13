a98b88cb7f9deb288db64baeb085bbf22bee84d9
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-13 20:16:57 +0530

    Used the password encoder in the UserService to encode a user's password before saving it. Also, returned the saved user back to the controller.

--- taskify/src/main/java/com/taskify/user/service/UserService.java
+++ taskify/src/main/java/com/taskify/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.taskify.user.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.taskify.entity.User;
@@ -8,12 +9,16 @@ import com.taskify.user.repository.UserRepository;
 @Service
 public class UserService {
 	private UserRepository userRepository;
+	private PasswordEncoder passwordEncoder;
 
-	public UserService(UserRepository userRepository) {
+	public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
 		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
 	}
 
-	public void save(User user) {
-		userRepository.save(user);
+	public User save(User user) {
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+		return userRepository.save(user);
 	}
 }
