7999b81c3e8c86495627f4f29418e73beaf8853c
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-14 19:16:42 +0530

    Added a GlobalExceptionHandler class that for now handles the UserNotFoundException by throwing a 404 NOT FOUND error

---
+++ taskify/src/main/java/com/taskify/globalexceptionhandler/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.taskify.globalexceptionhandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.taskify.user.exception.UserNotFoundException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	@ExceptionHandler(UserNotFoundException.class)
+	public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+	}
+}
