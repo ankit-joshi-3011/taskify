25c0a3a69e37d137aa202ae870c4df1aec7b8b6e
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-14 19:58:27 +0530

    Added the method that handles the occurrence of an InvalidPasswordException

--- taskify/src/main/java/com/taskify/globalexceptionhandler/GlobalExceptionHandler.java
+++ taskify/src/main/java/com/taskify/globalexceptionhandler/GlobalExceptionHandler.java
@@ -5,12 +5,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.taskify.user.exception.InvalidPasswordException;
 import com.taskify.user.exception.UserNotFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 	@ExceptionHandler(UserNotFoundException.class)
 	public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+	}
+
+	@ExceptionHandler(InvalidPasswordException.class)
+	public ResponseEntity<String> handleInvalidPasswordException(InvalidPasswordException ex) {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
 	}
 }
