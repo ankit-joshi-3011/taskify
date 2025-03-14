b68e73318a06fb9707deebd0abca94bc4701c0ef
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-14 21:54:24 +0530

    Added the @Setter annotation to the JwtConfiguration class so that while initializing the JwtConfiguration class, Spring Boot can set the appropriate values

--- taskify/src/main/java/com/taskify/security/jwt/JwtConfiguration.java
+++ taskify/src/main/java/com/taskify/security/jwt/JwtConfiguration.java
@@ -4,10 +4,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Component
 @ConfigurationProperties(prefix = "jwt")
 @Getter
+@Setter
 public class JwtConfiguration {
 	private String signaturePrivateKey;
 	private String signaturePublicKey;
