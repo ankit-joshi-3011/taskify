40df3865d252033e972321b6a5dba4a3427564c8
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-14 21:35:52 +0530

    Added a method to extract the username from the JWT when a request is received. Also, added a couple of internal methods to extract all claims and the relevant claim.

--- taskify/src/main/java/com/taskify/security/jwt/JwtUtility.java
+++ taskify/src/main/java/com/taskify/security/jwt/JwtUtility.java
@@ -3,9 +3,12 @@ package com.taskify.security.jwt;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.util.Date;
+import java.util.function.Function;
 
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.InvalidKeyException;
 import lombok.AllArgsConstructor;
@@ -27,5 +30,22 @@ public class JwtUtility {
 			.expiration(new Date(currentSystemTime + jwtConfiguration.getExpiration()))
 			.signWith(jwtKeyPairProvider.getPrivateKey(), Jwts.SIG.RS256)
 			.compact();
+	}
+
+	public String extractUsername(String jwt) throws JwtException, IllegalArgumentException, InvalidKeySpecException, NoSuchAlgorithmException {
+		return extractClaim(jwt, Claims::getSubject);
+	}
+
+	private <T> T extractClaim(String jwt, Function<Claims, T> claimsResolver) throws JwtException, IllegalArgumentException, InvalidKeySpecException, NoSuchAlgorithmException {
+		Claims claims = extractAllClaims(jwt);
+		return claimsResolver.apply(claims);
+	}
+
+	private Claims extractAllClaims(String jwt) throws JwtException, IllegalArgumentException, InvalidKeySpecException, NoSuchAlgorithmException {
+		return Jwts.parser()
+			.verifyWith(jwtKeyPairProvider.getPublicKey())
+			.build()
+			.parseSignedClaims(jwt)
+			.getPayload();
 	}
 }
