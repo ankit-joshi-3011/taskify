cfe9ddae85e0bf2dddab7d5820dadc2b45c22fb9
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-14 18:53:46 +0530

    Added the findByUsername() method to find a user by their username. This will be used for authentication purposes.

--- taskify/src/main/java/com/taskify/user/repository/UserRepository.java
+++ taskify/src/main/java/com/taskify/user/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.taskify.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.taskify.entity.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
+	Optional<User> findByUsername(String username);
 }
