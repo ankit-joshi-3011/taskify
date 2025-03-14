7d1e5508a1a4ba6cb4ef34d3d32c91e81ba06a6c
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-14 21:50:48 +0530

    Added the securityFilterChain bean to TaskifyConfiguration. This will: a) Disable CSRF since we are using JWT-based authentication. b) Make sure that the /api/users/register and the /api/auth/login endpoints can be accessed unauthenticated. c) Make sure that any other request apart from the two mentioned above will need to be authenticated. d) Not create a session e) Add the JwtFilter before the UsernamePasswordAuthenticationFilter for authentication purposes

--- taskify/src/main/java/com/taskify/configuration/TaskifyConfiguration.java
+++ taskify/src/main/java/com/taskify/configuration/TaskifyConfiguration.java
@@ -2,13 +2,37 @@ package com.taskify.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.taskify.security.jwt.filter.JwtFilter;
+
+import lombok.AllArgsConstructor;
 
 @Configuration
+@AllArgsConstructor
 public class TaskifyConfiguration {
+	private JwtFilter jwtFilter;
+
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+			.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers("/api/users/register").permitAll()
+				.requestMatchers("/api/auth/login").permitAll()
+				.anyRequest().authenticated())
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+			.build();
 	}
 }
