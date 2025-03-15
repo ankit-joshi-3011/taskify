ac05ed9c60c5ce0643cad7fd21861147161370c6
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-15 22:16:07 +0530

    Auto-formatted the User class

--- taskify/src/main/java/com/taskify/entity/User.java
+++ taskify/src/main/java/com/taskify/entity/User.java
@@ -21,13 +21,13 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	@Column(nullable = false, unique = true)
 	private String username;
-	
+
 	@Column(nullable = false, unique = true)
 	private String email;
-	
+
 	@Column(nullable = false)
 	private String password;
 }
